d7a62bcf4482d82b11ba615cc035eea6992a2117
Wallison Rocha <wallisonrocha2008@gmail.com>
2020-05-30 20:34:22 -0300

    Fisrt commit

---
+++ src/test/java/br/com/wallison/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wallison.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
